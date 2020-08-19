1141c2d96aaaa7cbb36e63efdfb2f7740aa5ffd5
github username <rdg.brg@gmail.com>
2020-08-19 19:17:12 -0400

    Users not rendering in NewProject component

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :job_title
+  attributes :id, :email, :first_name, :last_name, :job_title, :available
   has_many :projects
 end
