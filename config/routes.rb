addbbbfd4cb9fd971e0c9ed6b850bd44be5ec8df
github username <rdg.brg@gmail.com>
2020-12-05 18:25:39 -0500

    Add sessions controller for login and autologin

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
 
+      post '/login', to: 'sessions#login'
+      get '/autologin', to: 'sessions#autologin'
+
       resources :invites 
       resources :documents, only: [:index, :create]
       resources :archive_documents, only: [:index, :show, :create]
-    
+
       resources :users, only: [:index, :update, :show, :destroy]
       post '/signup', to: 'users#create'
-      post '/login', to: 'users#login'
-      get '/autologin', to: 'users#autologin'
       delete 'users/:user_id/remove-project/:project_id', to: 'users#remove_project'
     
       resources :archive_projects, only: [:index, :show, :create, :destroy]
